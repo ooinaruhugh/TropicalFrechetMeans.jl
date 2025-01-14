0f5a9d5bfca19e58d5eb15d0d5cefa8d6cad58d9
Kamillo Ferry <kafe@kafe.dev>
2025-01-14 11:02:18 +0100

    Add simple example

---
+++ examples/intro.jl
@@ -0,0 +1,15 @@
+using Polyhedron
+using TropicalFrechetMeans
+
+function trop_normalize(x)
+    return x .- first(x)
+end
+
+sample = [[-3,0,0], [0,-6,0], [0,0,-12]]
+
+num_FM = tropical_frechet_mean(sample) |> trop_normalize
+@show num_FM
+
+P = tropical_frechet_set(sample; tol=1e-3)
+@show P
+@show vrep(P)
