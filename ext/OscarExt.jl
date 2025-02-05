d029dfda9f459ae17e6ea02da28533a25809530c
Kamillo Ferry <kafe@kafe.dev>
2025-02-05 01:32:25 +0100

    Add function for weighted digraph polyhedron

--- ext/OscarExt.jl
+++ ext/OscarExt.jl
@@ -1,9 +1,20 @@
 module OscarExt
 
 using TropicalFrechetMeans
+using Polyhedra
 import Oscar: QQ, TropicalSemiringElem
 
 TropicalFrechetMeans.breakpoints_of_tropical_line(p::Vector{T}, q::Vector{T}) where {T<:TropicalSemiringElem} = breakpoints_of_tropical_line(QQ.(p), QQ.(q))
+
+function TropicalFrechetMeans.wdp_from_polyhedron(H::Polyhedron)
+    T = tropical_semiring()
+    C = identity_matrix(T, n)
+    for h in halfspaces(H)
+        setindex!(C, h.β, indices(h.a)...)
+    end
+
+    return C
+end
 
 
 end # module OscarExt
